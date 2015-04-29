956f636a19403d59a469d23dd66a86cde5e24679
neoluc <neoluc@hotmail.com>
2015-04-30 02:23:52 +0800

    updated ftp_client

--- src/ftp_client.h
+++ src/ftp_client.h
@@ -9,18 +9,7 @@
 #include <unistd.h>
 #include "ftp.h"
 
-enum FtpClientError {
-	FTP_CLIENT_ERROR_INVALID_INPUT = -1,
-	FTP_CLIENT_ERROR_OPEN_CONN = -2,
-	FTP_CLIENT_ERROR_AUTH = -3,
-	FTP_CLIENT_ERROR_LIST = -4,
-	FTP_CLIENT_ERROR_GET = -5,
-	FTP_CLIENT_ERROR_PUT = -6,
-	FTP_CLIENT_ERROR_QUIT = -7,
-};
-
 int ftp_client_fd;
-int ftp_client_state;
 
 int ftp_client_initialize();
 int ftp_client_connect_server(char *, char *);
@@ -31,6 +20,5 @@ int ftp_client_action_list(char *, int);
 int ftp_client_action_get(char *);
 int ftp_client_action_put(char *);
 int ftp_client_action_quit();
-// int ftp_client_process(char **, int);
 
 #endif
